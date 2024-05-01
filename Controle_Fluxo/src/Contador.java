8fecb5eabaf543c1aa2120a63b7e3e09578f85fa
jonas07br <jonasrafa07@gmail.com>
2024-05-01 11:56:13 -0300

    Update Contador

--- Controle_Fluxo/src/Contador.java
+++ Controle_Fluxo/src/Contador.java
@@ -1,3 +1,33 @@
+import java.util.Scanner;
+@SuppressWarnings("resource")
+
 public class Contador {
-    
+    static void contar(int parametroum,int parametrodois) throws ParametrosInvalidosException{
+        if(parametroum>parametrodois){
+            throw new ParametrosInvalidosException();
+        }
+        else{
+            int contagem = parametroum - parametrodois;
+            for(int x =0;x<contagem;x++){
+                System.out.println(x);
+            }
+        }
+        
+
+    }
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Digite o primeiro parâmetro:");
+        int parametroum = scan.nextInt();
+        System.out.print("Digite o segundo parâmetro:");
+        int parametrodois = scan.nextInt();
+        try{
+            contar(parametroum, parametrodois);
+        }
+        catch(ParametrosInvalidosException e){
+            System.out.println("O Parâmetro 1 é maior que o Parâmetro 2.");
+        }
+
+    }
 }
